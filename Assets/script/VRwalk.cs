9da4f0b675c1401547b830112749966363393760
adhi0302005 <adhi0302005@gmail.com>
2026-02-23 12:55:34 -0600

    bueno

---
+++ Assets/script/VRwalk.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    //Atributos /variables de clase
+
+    public Transform vrCamera;
+    public float angulo = 30.0f;
+    public float speed = 3.0f;
+    public bool move;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
